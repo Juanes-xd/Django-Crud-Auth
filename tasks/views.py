3e055bef127222ae4c099b0009f95dffc532d7db
Juanes-xd <juanestebanortizbejarano@gmail.com>
2025-08-07 15:51:30 -0500

    Add base template and update views for task management
    - Created base.html for consistent layout across pages.
    - Updated home.html, signup.html, and tasks.html to extend base template.
    - Enhanced signup view to handle errors and redirect upon successful registration.
    - Added tasks view to render tasks.html.

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
 # Create your views here.
 
 def home(request):
@@ -11,16 +12,19 @@ def signup(request):
     if request.method == 'GET':
         form = UserCreationForm()
         return render(request, 'signup.html', {'form': form})
-    
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.get(username=request.POST['username'])
-                return HttpResponse("Username already exists!")
+                form = UserCreationForm()
+                return render(request, 'signup.html', {'form': form, "error": "Username already exists!"})
             except User.DoesNotExist:
-                user = User.objects.create_user(username=request.POST['username'],
-                                                    password=request.POST['password1'])
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse(f"User {user.username} created successfully!")
-        return HttpResponse("Passwords do not match!")
-   
+                login(request, user)
+                return redirect('tasks')
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form, "error": "Passwords do not match."})
+
+def tasks(request):
+    return render(request, 'tasks.html')
