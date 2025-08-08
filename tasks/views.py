8525b83cb2eae238ccf40c6d48845f0d2991e9db
Juanes-xd <juanestebanortizbejarano@gmail.com>
2025-08-07 22:38:57 -0500

    Add sign-in and sign-out functionality, update task model and views

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 # Create your views here.
 
 def home(request):
@@ -26,5 +26,26 @@ def signup(request):
         form = UserCreationForm()
         return render(request, 'signup.html', {'form': form, "error": "Passwords do not match."})
 
+
+
+
 def tasks(request):
     return render(request, 'tasks.html')
+
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {'form': AuthenticationForm(), 'error': 'Username or password is incorrect'})
+        else:
+            login(request, user)
+            return redirect('tasks')
